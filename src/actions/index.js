880e7605b15e63507c25a2c15f35b5810c4f280f
Kristea Gamel <krgamel@gmail.com>
2019-04-16 20:27:02 -0500

    added imports, created actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
 // we'll need axios
-
+import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-
+export const FETCH_CHARS_START = 'FETCH_CHARS_START';
+export const FETCH_CHARS_SUCCESS = 'FETCH_CHARS_SUCCESS';
+export const FETCH_CHARS_FAIL = 'FETCH_CHARS_FAIL';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const fetch_chars = () => dispatch => {
+    dispatch({ type: FETCH_CHARS_START });
+    axios.get('https://swapi.co/api/people/')
+    .then(response => {dispatch({ type: FETCH_CHARS_SUCCESS, payload: response.data.message});}
+    )
+    .catch(err => {dispatch({ type: FETCH_CHARS_FAIL, payload: err});
+});
+}
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import rootReducer from "./reducers";
 // rootReducer from ./reducers
 
 const store = createStore(
-  rootReducer, applyMiddleware(thunk)
+  rootReducer, applyMiddleware(thunk, logger)
 );
 
 ReactDOM.render(
